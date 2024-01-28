f1edb997b946a10fee6a513f5ce1fa68974a3bba
Nicha <NichaRoj@gmail.com>
2024-01-28 22:34:36 +0700

    fix base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://egnarregnar/github.io/eg-commission-info'
+  base: '/eg-commission-info/'
 })
