b823ea15b40043f76f3ef1c4076f2de5630b41fe
Nicha <NichaRoj@gmail.com>
2024-01-30 21:35:02 +0700

    show commission types dynamically

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,33 @@ import IconTwitter from "~icons/formkit/twitter";
 function toTwitter() {
   window.open("https://twitter.com/egnarregnar", "_blank");
 }
+
+const stuff = [
+  {
+    key: "1",
+    name: "Normal Art",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/2560px-Image_created_with_a_mobile_phone.png",
+  },
+  {
+    key: "2",
+    name: "Manga",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/2560px-Image_created_with_a_mobile_phone.png",
+  },
+  {
+    key: "3",
+    name: "Discord Emotes",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/2560px-Image_created_with_a_mobile_phone.png",
+  },
+];
 </script>
 
 <template>
   <CardLayout headerImage="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/2560px-Image_created_with_a_mobile_phone.png">
     <template #content>
-      <CommissionTypesPage></CommissionTypesPage>
+      <CommissionTypesPage :choices="stuff"></CommissionTypesPage>
     </template>
     <template #footer>
       <div class="flex justify-center p-8">
--- src/pages/CommissionTypes.vue
+++ src/pages/CommissionTypes.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
 import Card from "../components/Card.vue";
+
+defineProps<{
+  choices: { key: string; name: string; image: string };
+}>();
 </script>
 
 <template>
@@ -7,20 +11,14 @@ import Card from "../components/Card.vue";
     <div class="flex justify-center px-8 pb-8">
       <p>Thank you for your interest! What kind of commission would you like to request?</p>
     </div>
-    <div class="px-16 grid grid-cols-1 sm:grid-cols-3 gap-4 justify-items-center">
-      <Card headerImage="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/2560px-Image_created_with_a_mobile_phone.png">
+    <div class="px-16 flex flex-wrap justify-center gap-4">
+      <Card
+        v-for="choice in choices"
+        :key="choice.key"
+        :headerImage="choice.image"
+      >
         <div class="flex p-8 justify-center">
-          <p>Normal Art</p>
-        </div>
-      </Card>
-      <Card headerImage="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/2560px-Image_created_with_a_mobile_phone.png">
-        <div class="flex p-8 justify-center">
-          <p>Manga</p>
-        </div>
-      </Card>
-      <Card headerImage="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/2560px-Image_created_with_a_mobile_phone.png">
-        <div class="flex p-8 justify-center">
-          <p>Discord Emotes</p>
+          <p>{{ choice.name }}</p>
         </div>
       </Card>
     </div>
