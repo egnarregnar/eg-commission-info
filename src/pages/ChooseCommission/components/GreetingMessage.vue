85ab273a880f82d6866689407eb8a321870d9946
Nicha <NichaRoj@gmail.com>
2024-03-08 21:08:51 +0700

    add markdown support

--- src/pages/ChooseCommission/components/GreetingMessage.vue
+++ src/pages/ChooseCommission/components/GreetingMessage.vue
@@ -2,7 +2,11 @@
 </script>
 
 <template>
-  <div class="flex justify-center px-8 pb-8">
+  <div class="flex justify-center items-center px-8 pb-8">
+    <img
+      class="w-16 border"
+      src="@assets/images/pog.png"
+    />
     <p>Thank you for your interest! What kind of commission would you like to request?</p>
   </div>
 </template>
--- src/pages/CommissionDetails/page.vue
+++ src/pages/CommissionDetails/page.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useCategoryStore } from "@/store/category";
 import { storeToRefs } from "pinia";
+import Markdown from "@/components/Markdown.vue";
 
 const store = useCategoryStore();
 const { chosenChoice } = storeToRefs(store);
@@ -8,9 +9,20 @@ const { resetChoice } = store;
 </script>
 
 <template>
-  <div class="flex-col justify-center p-8">
-    <p class="text-center">{{ chosenChoice?.name }}</p>
-    <p>{{ chosenChoice?.description }}</p>
+  <div class="flex-col justify-center py-8 px-16">
+    <button
+      @click="resetChoice"
+      class="font-bold"
+    >Back</button>
+    <p class="text-center text-2xl font-bold">{{ chosenChoice?.name }}</p>
+    <div class="flex justify-center items-center">
+      <img
+        class="w-16 border"
+        src="@assets/images/pog.png"
+      />
+      <Markdown :source="chosenChoice?.description" />
+    </div>
+
     <button
       @click="resetChoice"
       class="font-bold"
